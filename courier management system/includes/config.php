865d61494a935f3399ae7d5a2757f30a3878a9c9
UmerAbdali <umerabdali20510@gmail.com>
2026-05-30 00:16:49 +0500

    Delete courier management system directory

--- courier management system/includes/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-define('DB_FILE', __DIR__ . '/../data/database.sqlite');
-define('APP_NAME', 'Courier Management System');
--- courier management system/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/auth.php';
-logout_user();
-flash('You have been logged out.', 'success');
-redirect('index.php');
