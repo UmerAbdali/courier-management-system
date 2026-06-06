0845e80edc12d9f7fc0ef4d5ec84d4984bcb7407
UmerAbdali <umerabdali20510@gmail.com>
2026-06-06 11:38:10 +0500

    Delete Courier Management System directory

--- Courier Management System/admin/admin-sidebar.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const sidebarLinks = document.querySelectorAll('.admin-sidebar-nav a');
-    sidebarLinks.forEach(link => {
-        const linkPath = link.getAttribute('href');
-        if (window.location.pathname.endsWith(linkPath)) {
-            link.classList.add('active');
-        }
-    });
-});
